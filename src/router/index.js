bd62a01d9c1f23eb81ce3d496c20c713cf8581ad
Eliana <jedorado216@unicauca.edu.co>
2021-05-14 01:48:34 -0500

    login

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,15 @@ const routes = [
     name: "Detail",
     component: () => import("../views/Detail.vue"),
   },
+  {
+    path: "/login",
+    name: "Login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
+  },
 ];
 
 const router = new VueRouter({
